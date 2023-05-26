f14c5033e6dd780b7ebcaa1d339d0b6235d8cf5e
SajadZiaee <dayan.acorn@gmail.com>
2023-05-26 17:46:42 +0330

    solve 1.2

--- 1.2.js
+++ 1.2.js
@@ -1,1 +1,23 @@
 //2
+
+function delay() {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+    });
+}
+
+console.log("Start");
+
+delay()
+    .then(() => {
+        console.log("First Promise Resolved");
+        return delay(1000);
+    })
+    .then(() => {
+        console.log("Second Promise Resolved");
+        return delay(1000);
+    })
+    .then(() => {
+        console.log("Third Promise Resolved");
+        console.log("End");
+    });
