ee85ae455f638477f2d98f85dcf560f21427cc5b
SajadZiaee <dayan.acorn@gmail.com>
2023-05-26 17:36:16 +0330

    solve 1.3

--- 1.3.js
+++ 1.3.js
@@ -1,1 +1,16 @@
 //3
+
+function flatten(arr) {
+    const result = [];
+    arr.forEach((item) => {
+        if (Array.isArray(item)) {
+            result.push(...flatten(item));
+        } else {
+            result.push(item);
+        }
+    });
+    return result;
+}
+
+const test = [4, [[5], [6, [7], 8], 9, 10]];
+console.log(flatten(test));
