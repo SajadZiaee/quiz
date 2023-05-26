b7ebf876df634d46ec1a4cff284867d8387127ae
SajadZiaee <dayan.acorn@gmail.com>
2023-05-26 17:40:59 +0330

    solve 1.4

--- 1.4.js
+++ 1.4.js
@@ -1,1 +1,19 @@
 //4
+
+// Object:
+const originalObject = {
+    name: 'Sajad',
+    lastName: 'Ziaee'
+};
+
+const proxyHandler = {
+    get(target, property) {
+        return "404";
+    }
+};
+
+// Proxy:
+const proxyObject = new Proxy(originalObject, proxyHandler);
+
+console.log(proxyObject.name);
+console.log(proxyObject.lastName);
